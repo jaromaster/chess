d6cfd8a4f195adff5a4bae0f7543e444b1fc775d
jaromaster <jaromirblu@gmail.com>
2021-11-24 21:12:29 +0100

    implemented two step move for pawns

--- Pieces/Pawn.py
+++ Pieces/Pawn.py
@@ -4,6 +4,7 @@ class Pawn(ChessPiece):
     """ Pawn is the least powerful chess piece """
     def __init__(self, color, grid, pos, screen, image):
         super().__init__(color, grid, pos, screen, image)
+        self.isFirstMove = True
 
     # checks for friendly chess pieces and enemy chess pieces
     def validMoves(self):
@@ -15,6 +16,10 @@ class Pawn(ChessPiece):
             # directly above pawn if empty
             if self.inBounds(x, y-1) and self.grid[y-1, x] == None: 
                 moves.append((x, y-1))
+            # two steps above (only on first move)
+            if self.inBounds(x, y-2) and self.grid[y-2, x] == None and self.isFirstMove: 
+                moves.append((x, y-2))
+                self.isFirstMove = False
             # above pawn (left) if not empty and enemy chess piece
             if self.inBounds(x-1, y-1) and self.grid[y-1, x-1] and self.grid[y-1, x-1].color != "white": 
                 moves.append((x-1,y-1))
@@ -26,6 +31,10 @@ class Pawn(ChessPiece):
             # directly below pawn if empty
             if self.inBounds(x, y+1) and self.grid[y+1, x] == None: 
                 moves.append((x, y+1))
+            # two steps below (only on first move)
+            if self.inBounds(x, y+2) and self.grid[y+2, x] == None and self.isFirstMove: 
+                moves.append((x, y+2))
+                self.isFirstMove = False
             # below pawn (left) if not empty and enemy chess piece
             if self.inBounds(x-1, y+1) and self.grid[y+1, x-1] and self.grid[y+1, x-1].color == "white": 
                 moves.append((x-1,y+1))
