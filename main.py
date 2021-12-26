38141416c48e656a43368718c5cc53d7932ac668
jaromaster <jaromirblu@gmail.com>
2021-12-26 13:44:36 +0100

    restarting, improved texts

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def initWindow(width, height, title, color) -> pygame.Surface:
 
     return screen
 
+# write text with specific font to specific position 
 def writeText(string, xCenter, yCenter, font, screen):
     """ draw text to canvas, call pygame.display.update() to update screen """
     winnerText = font.render(string, True, (0,0,0), (255,255,255)) # tuples are colors of text and background
@@ -28,8 +29,7 @@ def main():
     squareColor = (111, 73, 73)
     squareLength = 80
     fontSize = 30
-    font = pygame.font.Font("freesansbold.ttf", fontSize)
-    
+    font = pygame.font.Font("freesansbold.ttf", fontSize) # create font
     
     # initialize game window
     screen = initWindow(width, height, title, background_color) 
@@ -44,6 +44,7 @@ def main():
     turn = "white"
     mouseCol, mouseRow = 0, 0
     moves = set() # possible moves of selected chess piece
+    won = False # disable clicking if player won
     # game loop
     pygame.event.set_allowed([pygame.QUIT, pygame.MOUSEBUTTONDOWN, pygame.KEYDOWN])
     while isRunning:
@@ -51,14 +52,17 @@ def main():
             if event.type == pygame.QUIT: # break if quit event occurred
                 isRunning = False
 
-            elif event.type == pygame.KEYDOWN: # restart game by pressing space
+            elif event.type == pygame.KEYDOWN: 
+                # restart game by pressing space
                 if event.key == pygame.K_SPACE:
-                    gameBoard.drawGrid()
+                    won = False
+                    turn = "white"
+                    screen.fill(pygame.Color("white")) # redraw background
                     gameBoard.initChessPieces()
+                    gameBoard.drawGrid()
                     gameBoard.drawChessPieces()
-                    print("redrawn") # testing
 
-            elif event.type == pygame.MOUSEBUTTONDOWN: # user clicked
+            elif event.type == pygame.MOUSEBUTTONDOWN and not won: # user clicked, disable if player already won
                 # convert coordinates of mouse to rows and columns of grid
                 mouseCol = int((pygame.mouse.get_pos()[0] - gameBoard.xStart) // squareLength)
                 mouseRow = int((pygame.mouse.get_pos()[1] - gameBoard.yStart) // squareLength)
@@ -72,17 +76,16 @@ def main():
 
                         # check for wins after every move
                         bothAlive, colorDead = gameBoard.checkBothKingsAlive()
-                        if not bothAlive:
-                            print("King dead") # testing
-
+                        if not bothAlive: # if king dead
+                            won = True
 
                             # display winner text
+                            textFieldHeight = (height - 8*squareLength) // 2 # center text vertically in area outside grid
                             color = "black" if colorDead == "white" else "white"
-                            writeText("Player "+color+" won", width // 2, fontSize, font, screen)
+                            writeText(color.capitalize()+" won", width // 2, textFieldHeight // 2, font, screen)
+                            # display text explaining how to restart
+                            writeText("Press SPACE to restart", width // 2, height - textFieldHeight // 2, font, screen)
                             pygame.display.update()
-
-                            #todo: restart button
-                            #todo: restart game on button click
 
                         moves = set() # reset moves
                         turn = "white" if turn == "black" else "black" # switch color
